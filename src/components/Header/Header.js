282c339d469877a5e7ad95c3baed0c5fc3356035
Dax Wann <daxwann@gmail.com>
2020-09-07 11:28:54 -0700

    set up redux actions, reducers, and store

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './styles.css';
 
-const Header = () => <header>What the Splash!</header>;
+const Header = () => <header>Unsplash w/ Redux Saga</header>;
 
 export default Header;
